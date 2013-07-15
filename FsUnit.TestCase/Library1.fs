7544d6bfbb91689bb8cabaed51d8ce12f6480da1
mavnn <michael@mavnn.co.uk>
2013-07-15 10:14:31 +0100

    Initial commit

---
+++ FsUnit.TestCase/Library1.fs
@@ -0,0 +1,19 @@
+﻿module Tests
+
+open NUnit.Framework
+open FsUnit
+
+[<TestCase(1, 2, Result = 3)>]
+let Adder x y =
+    x + y
+
+let testCaseSource =
+    [|
+        [| 2; 3; 6 |]
+        [| 4; 5; 20 |]
+    |]
+
+[<Test>]
+[<TestCaseSource("testCaseSource")>]
+let Multiplier x y result =
+    x * y |> should equal result
